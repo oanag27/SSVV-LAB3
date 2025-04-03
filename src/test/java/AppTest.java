075009d4d49f1e0470bd6ea79c1ecb25492f8053
913-Gog-Oana <oana.gog@stud.ubbcluj.ro>
2025-04-03 14:59:53 +0300

    Add new test cases

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,14 +1,15 @@
 import domain.Student;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import repository.StudentFileRepository;
 import repository.StudentXMLRepository;
 import service.Service;
 import validation.StudentValidator;
 import validation.Validator;
 
+import java.io.File;
+import java.io.PrintWriter;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class AppTest {
 
@@ -38,5 +39,38 @@ class AppTest {
         int invalidGroup = 100;
         int result = service.saveStudent(studentId, studentName, invalidGroup);
         assertEquals(1, result, "Student with invalid input should not be saved");
+    }
+    @Test
+    void addStudentWithEmptyNameTest() {
+        Validator<Student> validator = new StudentValidator();
+        StudentXMLRepository repository = new StudentXMLRepository(validator, "studenti.xml");
+        Service service = new Service(repository, null, null);
+        String studentId = "20001";
+        String emptyName = "";
+        int group = 120;
+        int result = service.saveStudent(studentId, emptyName, group);
+        assertEquals(1, result, "Student with empty name should not be saved");
+    }
+    @Test
+    void addStudentWithNullIdTest() {
+        Validator<Student> validator = new StudentValidator();
+        StudentXMLRepository repository = new StudentXMLRepository(validator, "studenti.xml");
+        Service service = new Service(repository, null, null);
+        String nullId = null;
+        String name = "Ana";
+        int group = 120;
+        int result = service.saveStudent(nullId, name, group);
+        assertEquals(1, result, "Student with null ID should not be saved");
+    }
+    @Test
+    void addStudentWithNegativeGroupTest() {
+        Validator<Student> validator = new StudentValidator();
+        StudentXMLRepository repository = new StudentXMLRepository(validator, "studenti.xml");
+        Service service = new Service(repository, null, null);
+        String studentId = "30001";
+        String name = "Ion";
+        int negativeGroup = -5;
+        int result = service.saveStudent(studentId, name, negativeGroup);
+        assertEquals(1, result, "Student with negative group number should not be saved");
     }
 }
